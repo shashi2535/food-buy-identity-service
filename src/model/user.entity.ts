0c0e814d004f7f7642ef3d822ab9d0fe0ac6ecb7
shashikant <shashikant.thoghtwin@gmail.com>
2023-10-23 16:28:19 +0530

    fix(scope): add user table model

--- src/model/user.entity.ts
+++ src/model/user.entity.ts
@@ -1,13 +1,35 @@
-import { Column, Model, Table } from 'sequelize-typescript';
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
 
-@Table
+@Table({
+  tableName: 'users',
+  underscored: true,
+  timestamps: true,
+})
 export class User extends Model<User> {
+  @Column({
+    primaryKey: true,
+  })
+  id: number;
+  @Column({
+    unique: true,
+  })
+  email: string;
   @Column
-  firstName: string;
-
+  name: string;
   @Column
-  lastName: string;
-
+  phone: string;
+  @Column
+  password: string;
+  @Column
+  otp: string;
+  @Column
+  token: string;
+  @Column({ type: DataType.DATE })
+  otpExpire: Date;
+  @Column({ type: DataType.DATE })
+  tokenExp: Date;
+  @Column({ defaultValue: false })
+  isVerified: boolean;
   @Column({ defaultValue: true })
   isActive: boolean;
 }
